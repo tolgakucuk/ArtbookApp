d0227f448a7e10f0fcfd6e893e1146705378fe85
tolgakucuk <68082820+tolgakucuk@users.noreply.github.com>
2021-05-05 17:14:29 +0300

    Initial Commit

---
+++ ArtbookApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtbookApp
+//
+//  Created by Tolga on 5.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
